dbba0d0e1fd92447b516b31aa602a9e336e87a73
Michael Chamberlain <cyberproject@lead.org.uk>
2024-11-14 14:50:58 +0000

    analytics

--- web/src/components/router-head/router-head.tsx
+++ web/src/components/router-head/router-head.tsx
@@ -50,6 +50,7 @@ export const RouterHead = component$(() => {
         <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
       ))}
       <script defer data-domain="digital-defense.io" src="https://no-track.as93.net/js/script.js"></script>
+      <Analytics />
     </>
   );
 });
