dc4330e4a835c563a22c0a9f731087505a90350d
Michael Chamberlain <cyberproject@lead.org.uk>
2024-11-14 15:37:55 +0000

    image nav.tsx

--- web/src/components/furniture/nav.tsx
+++ web/src/components/furniture/nav.tsx
@@ -35,7 +35,8 @@ export default component$(() => {
         <div class="flex-1">
           <div class="flex-none md:hidden">
             <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              <img src="../../media/thunder.png">
+              </img>
             </label>
           </div> 
           <a href="/" class="btn btn-ghost text-xl flex capitalize">
